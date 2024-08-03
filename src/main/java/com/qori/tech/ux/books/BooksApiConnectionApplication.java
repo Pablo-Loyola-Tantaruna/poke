1e1cc249e5bd9dc9cd94ba92158d345283eb628d
Ployola <ployola@hiper.com.pe>
2024-08-03 01:06:36 -0500

    chore: add new proyect to Thiago

---
+++ src/main/java/com/qori/tech/ux/books/BooksApiConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.qori.tech.ux.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qori/tech/ux/books/BooksApiConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qori.tech.ux.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
